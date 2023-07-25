1075d7cc3eaa745f3d8e073acea3b02e048f44f2
sagarn98 <89286018+sagarn98@users.noreply.github.com>
2023-07-25 18:36:14 +0530

    Create UserModule.java

---
+++ Actitime/src/com/actitime/testscript/UserModule.java
@@ -0,0 +1,9 @@
+public class UserModule extends BaseClass{
+@Test
+public void testCreateUser()  {
+	HomePage h=new HomePage(driver);
+	TaskListPage t=new TaskListPage(driver);
+	FileLib f=new FileLib();
+
+}
+}
