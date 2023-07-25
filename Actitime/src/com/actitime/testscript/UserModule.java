94be9ef2762361cc45946d336e95af0cb83e6afc
sagarn98 <89286018+sagarn98@users.noreply.github.com>
2023-07-25 18:47:56 +0530

    Update UserModule.java

--- Actitime/src/com/actitime/testscript/UserModule.java
+++ Actitime/src/com/actitime/testscript/UserModule.java
@@ -1,9 +1,6 @@
-public class UserModule extends BaseClass{
+public class UserModule {
 @Test
 public void testCreateUser()  {
-	HomePage h=new HomePage(driver);
-	TaskListPage t=new TaskListPage(driver);
-	FileLib f=new FileLib();
-//
+	
 }
 }
